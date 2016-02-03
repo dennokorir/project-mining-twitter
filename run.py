29c9423b20b8f6200c37226dac58617dca4663ef
Denis Korir <dennokorir@gmail.com>
2016-02-03 21:02:39 +0300

    Optimized alchemyapi sentiment analysis Reduced the analysis scope of the data to be analyzed by the alchemy sentiment analysis api to the top list of words resulting from the ranking. This provides a more accurate feel of the words on display

--- run.py
+++ run.py
@@ -16,15 +16,10 @@ def search(query):
 
     search_results = twitter_api.search.tweets(q = q , count = count, lang = 'en', result_type = 'recent', screen_name = query)
 
-
     statuses = search_results['statuses']
-
     status_texts = [status['text'] for status in statuses]
-
     summary = [status.replace("\"","") for status in status_texts]
-
     summary = "".join(status_texts).split(" ")
-
     summary = clean(summary)#remove stop words
 
     if summary:
@@ -35,11 +30,13 @@ def search(query):
     display()
 
 def clean(param):
+    #function to remove stop words
     print('Cleaning...'),
     sys.stdout.flush()
     stop_words = open('stop_words_lib.txt','r').readlines()
     stop_words = "".join(stop_words).split(",")
     param = [word for word in param if word.lower() not in stop_words]
+    param = [word for word in param if '@' not in word]
     return param
 
 def display(limit = 10):
@@ -52,7 +49,7 @@ def display(limit = 10):
     data_file.close()
     data = [word.encode('utf-8') for word in data]
 
-    #remove file after display finishes
+    #remove file after retrieving data
     os.remove('data.json')
 
 
@@ -70,36 +67,15 @@ def display(limit = 10):
         print('Catching feelings...'),
         sys.stdout.flush()
 
+        #process only the data in the list
+        sentiment_data = [key[0] for key in c.most_common()][:limit]#collect top ranked list
         from alchemyapi import AlchemyAPI
         alchemyapi = AlchemyAPI()
-        Text = "".join([word.decode() for word in data])
+        Text = "".join([word.decode() for word in sentiment_data])
         try:
             response = alchemyapi.sentiment("text",Text)#
             print("Sentiment: ", response["docSentiment"]["type"])
         except:
             print("No result to analyse")
 
-'''
-def user_exit_input():
-    command = input("Press Q to quit and any other key to continue \t")
-    if command.lower() == 'q':
 
-        return True
-
-def search_query():
-    search_string = ''
-    while len(search_string)<=0:
-        search_string = input("Who are we stalking? \t Q for exit \t")
-    search(search_string)
-
-
-#onstart
-while True:
-
-    search_query()
-
-    if user_exit_input():#Always provide an option to quit
-        print('Exiting...')
-        break
-
-'''
