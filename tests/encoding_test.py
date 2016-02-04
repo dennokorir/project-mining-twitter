9243b5f816d823270cda1670f866fc24b640da65
Denis Korir <dennokorir@gmail.com>
2016-02-04 20:49:30 +0300

    Changed folder structure to be compliant with best practise

--- tests/encoding_test.py
+++
@@ -1,6 +0,0 @@
-#encoding test
-import sys
-
-print(sys.stdout.encoding)
-print(u"Stöcker".encode(sys.stdout.encoding, errors='replace'))
-print(u"Стоескер".encode(sys.stdout.encoding, errors='replace'))
--- tests/mining_example.py
+++
@@ -1,4 +0,0 @@
-import oauth
-
-twitter = oauth.authenticate()
-print(twitter)
